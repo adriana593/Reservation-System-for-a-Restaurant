d7e1e72b7f6ee926ed6904e6ef550caaf5817753
dani0643 <dri895014@gmail.com>
2025-06-25 17:30:09 -0400

    doc: SOLID principles comments

--- EStatus.java
+++ EStatus.java
@@ -1,10 +1,12 @@
 package Domain;
+
+//El enum encapsula los estados posibles de una reserva. Es parte del diseño que ayuda a hacer el código más legible y reutilizable, facilitando el cumplimiento SRP
 public enum EStatus {
     Pendiente ("Pendiente"),
     Confirmada ("Confirmada"),
     Cancelada ("Cancelada");
     
-    
+    //Almacena la condición del estado
     public String condition;
     
     private EStatus(String condition){
