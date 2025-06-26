05471164af7313ce993db3ee1b59f098afdaead8
Adancb <adancastrobaez@gmail.com>
2025-06-26 04:10:57 +0200

    test: add update reservation test

--- RestaurantServiceTest.java
+++ RestaurantServiceTest.java
@@ -21,6 +21,27 @@ public class RestaurantServiceTest {
         // Se valida que la lista de reservas ya no esté vacía y que los datos de la reserva agregada sean correctos.
         assertFalse(service.getReservations().isEmpty());
         assertTrue(service.getReservations().contains(reservation));
+     }    
+   @Test
+    public void testUpdateReservation() {
+        // Se crea y agrega una nueva reserva.
+        Reservation reservation = new Reservation("2", "Ana Gomez", 3, new Date(28, 2, 2025) , new Time(14, 30));
+        service.addReservation(reservation);
         
+        // Se definen nuevos datos para actualizar la reserva.
+        String newName = "Ana Actualizada";
+        int newNumber = 5;
+        Date date = new Date(2025, 2, 30);
+        Time time = new Time(12, 40);
+        
+        // Se actualiza la reserva.
+        service.updateReservation("2", newName, newNumber, date, time);
+        
+        // Se recupera la reserva actualizada.
+        Reservation updated = service.getReservations().get(0);
+        assertEquals(newName, updated.getCustomerName());
+        assertEquals(newNumber, updated.getNumberOfPeople());
+        assertEquals(date, updated.getDate());
+        assertEquals(time, updated.getTime());
     } 
   } 
